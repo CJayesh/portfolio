783e88491a785d36b99112f19b02d537b798b2c3
Jayesh <chavan.jayesh63@gmail.com>
2023-04-04 11:31:09 +0200

    add smooth scroll

--- my-portfolio/src/components/Layout.tsx
+++ my-portfolio/src/components/Layout.tsx
@@ -9,7 +9,7 @@ function Layout({children}: Props) {
   return (
     <>
         <Navbar />
-        <div id='content' className='h-screen flex items-center justify-center pl-80 pr-40 flex-wrap overflow-auto'>
+        <div id='content' className='h-screen flex items-center justify-center pl-80 pr-40 flex-wrap overflow-auto scroll-smooth'>
             <main>{children}</main>    
         </div>
     </>
