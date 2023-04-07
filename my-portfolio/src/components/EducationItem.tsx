c83e36b21c6e584d0306ab1be24f51f17f45ddf9
Jayesh <chavan.jayesh63@gmail.com>
2023-04-08 00:21:19 +0200

    responsive layout

--- my-portfolio/src/components/EducationItem.tsx
+++ my-portfolio/src/components/EducationItem.tsx
@@ -10,7 +10,7 @@ export type Props = {
 
 function EducationItem(data: Props) {
     return (
-        <div className='flex flex-wrap rounded-lg mb-8 px-8 py-6 bg-white shadow-md hover:shadow-lg w-1/2'>
+        <div className='flex flex-wrap rounded-lg mb-8 px-2 py-3 min-w-[280px] max-w-xl sm:px-8 sm:py-6 md:w-96 lg:w-[640px] bg-white shadow-md hover:shadow-lg w-1/2'>
             <div className='w-full flex-[0_0_50%] font-bold'>{data.uni_name}</div>
             <div className='w-full flex-[0_0_50%] text-right text-sm italic'>{data.location}</div>
             <div className='w-full flex-[0_0_50%] font-semibold'>{data.degree}</div>
--- my-portfolio/src/components/WorkExpItem.tsx
+++ my-portfolio/src/components/WorkExpItem.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 function WorkExpItem({ company_name, duration, role, location, tasks }: Props) {
     return (
-        <div className='flex flex-wrap rounded-lg mb-8 px-8 py-6 bg-white shadow-md hover:shadow-lg w-1/2'>
+        <div className='flex flex-wrap rounded-lg mb-8 px-2 py-3 min-w-[280px] max-w-xl sm:px-8 sm:py-6 md:w-96 lg:w-[640px] bg-white shadow-md hover:shadow-lg w-1/2'>
             <div className='w-full flex-[0_0_50%] font-bold'>{role}</div>
             <div className='w-full flex-[0_0_50%] text-right text-sm italic'>{location}</div>
             <div className='w-full flex-[0_0_50%] font-semibold'>{company_name}</div>
