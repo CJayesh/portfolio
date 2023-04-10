76fe8e9a7fce199bca0c05c24f7be5a1d9855c7e
Jayesh <chavan.jayesh63@gmail.com>
2023-04-10 18:55:07 +0200

    UI changes

--- my-portfolio/src/pages/_app.tsx
+++ my-portfolio/src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import { Fredoka } from 'next/font/google'
+
+const font_heading = Fredoka({subsets: ['latin']})
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <style jsx global>
+            {`
+                html {
+                    font-family: ${font_heading.style.fontFamily}
+                }
+            `}
+            </style>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
--- my-portfolio/src/pages/experience.tsx
+++ my-portfolio/src/pages/experience.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import WorkExpItem from '@/components/WorkExpItem'
 import {
-    SiHtml5, SiCss3, SiJavascript, SiTypescript, SiAngular, SiReact, SiNextdotjs, SiPython, SiPytest, SiDjango,
-    SiRabbitmq, SiMysql, SiMongodb, SiJira, SiMiro, SiRedmine, SiAmazonaws, SiLinux, SiDocker, SiGit, SiGitlab, SiPycharm,
-    SiSass, SiTailwindcss
+    SiHtml5, SiCss3, SiTypescript, SiAngular, SiReact, SiPython, SiPytest, SiDjango,
+    SiRabbitmq, SiMysql, SiMongodb, SiJira, SiAmazonaws, SiLinux, SiDocker, SiGit, SiGitlab, SiSass
 } from 'react-icons/si'
 import { TbBrandVscode } from 'react-icons/tb'
 
@@ -16,18 +15,23 @@ const data = [
         location: 'Mumbai, India',
         duration: 'Nov 2021-Sep 2022',
         role: 'SDE 2',
-        tasks: [<SiPython key='py' className={icon_style}></SiPython>, <SiReact key='re' className={icon_style}></SiReact>,
-        <SiDjango key='dj' className={icon_style}></SiDjango>, <SiDocker key='dk' className={icon_style}></SiDocker>,
-        <SiGit key='git' className={icon_style}></SiGit>, <SiJira key='jira' className={icon_style}></SiJira>]
+        tasks: [<SiPython key='py' title='Python3' className={icon_style}></SiPython>,<SiReact key='re' title='React' className={icon_style}></SiReact>,
+        <SiDjango key='dj' title='Django' className={icon_style}></SiDjango>, <SiDocker key='dk' title='Docker' className={icon_style}></SiDocker>,
+        <SiGit key='git' title='Git' className={icon_style}></SiGit>, <SiGitlab key='gl' title='GitLab' className={icon_style}></SiGitlab>,
+        <SiPytest key='pt' title='PyTest' className={icon_style}></SiPytest>, <SiMongodb key='mdb' title='MongoDb' className={icon_style}></SiMongodb>,
+        <SiRabbitmq key='rmq' title='RabbitMQ' className={icon_style}></SiRabbitmq>, <SiJira key='jira' title='Jira' className={icon_style}></SiJira>]
     },
     {
         company_name: 'Zeus Learning',
         location: 'Mumbai, India',
         duration: 'Jun 2019-Nov 2021',
         role: 'Software Engineer',
-        tasks: [<SiHtml5 key='html' className={icon_style}></SiHtml5>, <SiCss3 key='css' className={icon_style}></SiCss3>,
-        <SiAngular key='ag' className={icon_style}></SiAngular>, <SiAmazonaws key='aws' className={icon_style}></SiAmazonaws>,
-        <SiGitlab key='gl' className={icon_style}></SiGitlab>, <SiLinux key='linux' className={icon_style}></SiLinux>]
+        tasks: [<SiHtml5 key='html' title='HTML5' className={icon_style}></SiHtml5>, <SiCss3 key='css' title='CSS3' className={icon_style}></SiCss3>,
+        <SiTypescript key='ts' title='Typescript' className={icon_style}></SiTypescript>,
+        <SiGitlab key='gl' title='GitLab' className={icon_style}></SiGitlab>,
+        <SiAngular key='ag' title='Angular' className={icon_style}></SiAngular>, <SiSass key='sass' title='SASS' className={icon_style}></SiSass>,
+        <SiPython key='py' title='Python3' className={icon_style}></SiPython>, <SiAmazonaws key='aws' title='AWS' className={icon_style}></SiAmazonaws>,
+        <SiMysql key='msql' title='MySQL' className={icon_style}></SiMysql>,<SiLinux key='linux' title='Linux' className={icon_style}></SiLinux>]
     }
 ]
 
